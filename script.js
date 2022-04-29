682bfd093ba347ef4c53c2fdcbdf618d7a97a7a9
Lynn Nijboer <lynnnijboer12@gmail.com>
2022-04-29 14:17:25 +0200

    errorr

--- script.js
+++
@@ -1,7 +0,0 @@
-const current = 0;
-for (var i = 0; i < document.links.length; i++) {
-    if (document.links[i].href === document.URL) {
-        current = i;
-    }
-}
-document.links[current].className = 'current';
